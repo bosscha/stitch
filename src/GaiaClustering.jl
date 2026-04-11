44d9270d353953c77478809c7a090aa6294912d7
Stephane <stephane@example.com>
2026-04-11 11:22:06 +0200

    chore: bump package version to 2.0.0

--- src/GaiaClustering.jl
+++ src/GaiaClustering.jl
@@ -28,7 +28,7 @@ import GeometryBasics as gb
 const Normal = Distributions.Normal
 const Rectangle = GeometryBasics.Rect2{Float64}
 
-VERSION= "1.8.1" 
+VERSION= "2.0.0" 
 
 ## include all the types
 include("types.jl")
